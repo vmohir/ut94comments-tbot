e4fa28f6740157b1d7cefcced396f7e4dc24f88e
Vahid Mohammadi <vahidm1467@gmail.com>
2019-05-30 18:45:22 +0430

    gitignore

--- config.ts
+++
@@ -1,4 +0,0 @@
-export const TOKEN = '887020773:AAGYU3EfA-f3xkNGDhDYekwuTZxi0A6Snoc';
-// export const CHANNEL_ID = '@EDMusics_bot';
-export const BOT_ADMIN_ID = '123405638';
-// export const BOT_ADMIN_ID = '92454';
--- dist/config.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TOKEN = '887020773:AAGYU3EfA-f3xkNGDhDYekwuTZxi0A6Snoc';
-// export const CHANNEL_ID = '@EDMusics_bot';
-exports.BOT_ADMIN_ID = '123405638';
-//# sourceMappingURL=config.js.map
